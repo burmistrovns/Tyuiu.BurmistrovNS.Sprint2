c20420952115aedb7ad09f0b0a7dc4ac1e168b70
Николай Бурмистров <Tyuiu.BurmistrovNS@outlook.com>
2025-09-27 23:37:56 +0500

    Task1

---
+++ Tyuiu.BurmistrovNS.Sprint2.Task1.V19.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+
+namespace Tyuiu.BurmistrovNS.Sprint2.Task1.V19.Lib
+{
+    public class DataService : ISprint2Task1V19
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = c == b + 69 | a > c;
+            res[1] = a != b & c == b;
+            res[2] = b < c || b != a;
+            res[3] = b != a && c > d;
+            res[4] = !(res[1]);
+            res[5] = a >= b ^ b+69 ==c;
+
+            return res;
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.BurmistrovNS.Sprint2.Task1.V19.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.BurmistrovNS.Sprint2.Task1.V19.Lib;
+
+namespace Tyuiu.BurmistrovNS.Sprint2.Task1.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetLogicOperations()
+        {
+            DataService ds = new DataService();
+            int a = 696;
+            int b = 354;
+            int c = 423;
+            int d = 957;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            bool[] wait = new bool[6] { true, false, true, false, true, false };
+
+            CollectionAssert.AreEqual(wait, res);
+
+
+
+        }
+    }
+}
